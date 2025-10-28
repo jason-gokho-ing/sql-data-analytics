3b644460d4d2c703520c8fa2142fce6cbbc826fa
jason gokho ing <jason.gokho.ing@gmail.com>
2025-10-28 15:25:16 -0700

    commit

--- scripts/key_metrics.sql
+++ scripts/key_metrics.sql
@@ -228,3 +228,56 @@ FROM category_sales;
 
 -- Data Segmentation
 
+/*Group customers into three segments based on their spending behavior:
+	- VIP: Customers with at least 12 months of history and spending more than $5,000.
+	- Regular: Customers with at least 12 months of history but spending $5,000 or less.
+	- New: Customers with a lifespan less than 12 months.
+And find the total number of customers by each group
+*/
+
+
+WITH customer_history AS (
+
+SELECT 
+    ci.customer_key AS customer_key, 
+    SUM(s.sales_amount) AS total_spending, 
+    DATEDIFF(month, MIN(s.order_date),MAX(s.order_date)) AS customer_lifespan
+FROM gold.dim_customer_info ci
+LEFT JOIN gold.fact_sales s ON ci.customer_id = s.customer_key
+GROUP BY ci.customer_key
+)
+
+
+SELECT customer_segment, COUNT(customer_key) AS total_customers FROM
+    (
+
+    SELECT customer_key, total_spending, customer_lifespan,
+            CASE
+                WHEN customer_lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
+                WHEN customer_lifespan >= 12 AND total_spending <= 5000 THEN 'Regular'
+                ELSE 'New'
+            END AS customer_segment
+        FROM customer_history
+
+    ) t
+GROUP BY customer_segment
+ORDER BY total_customers DESC;
+
+
+-- 
+SELECT cost_range, COUNT(*) [# of Customers] FROM (
+
+SELECT product_number, product_name, cost,
+    CASE
+        WHEN cost < 100 THEN 'Below 100'
+        WHEN cost BETWEEN 100 AND 500 THEN '100-500'
+        WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
+        ELSE 'Above 1000'
+    END AS cost_range
+FROM gold.dim_product_info pr
+GROUP BY product_number, product_name, cost
+) t
+
+GROUP BY cost_range;
+
+
